a13b37265ad12a473ab2a81435a2ede73278db18
SadEast <shapa2772@yandex.ru>
2023-03-31 03:09:27 +0300

    task3 solution

--- task3.java
+++ task3.java
@@ -1,5 +1,34 @@
 /*Task_3.
 Реализовать простой калькулятор (+,-,/,*), только с целыми числами.*/
+import java.util.Scanner;
 public class task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первое число:  ");
+        int num1 = sc.nextInt();
+        System.out.print("Введите операцию:  ");
+        String action = sc.next();
+        System.out.print("Введите второе число:  ");
+        int num2 = sc.nextInt();
+        switch (action) {
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                double a = num1;
+                System.out.println(a / num2);
+                break;
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            default:
+                System.out.println("Неверная операция");
+                break;
+        }
+        sc.close();        
+    }
     
 }
