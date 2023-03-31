1c4b5e577c0ae35023babd5260a26ff4855fec8d
SadEast <shapa2772@yandex.ru>
2023-03-31 03:05:27 +0300

    task2 solution

--- task2.java
+++ task2.java
@@ -15,3 +15,22 @@ Sample Output:
 6
 10*/
 
+import java.util.Scanner;
+public class task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число n =  ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i <n; i++) {
+            System.out.print("Введите число:  ");
+            array[i] = sc.nextInt();
+        }
+        System.out.print("Введите множитель:  ");
+        int num = sc.nextInt();
+        for (int i : array) {
+            System.out.println(i * num);
+        }
+        sc.close();
+    }
+}
